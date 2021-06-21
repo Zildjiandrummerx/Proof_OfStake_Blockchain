17181c8e5b9b7df50c56cdc480c33220a936e2b9
Yotch <joshua.rg18@gmail.com>
2021-06-21 15:28:26 -0600

    Finished the Interaction API

--- node.py
+++ node.py
@@ -1,8 +1,10 @@
+from blockchain import Blockchain
 from transactionPool import TransactionPool
 from wallet import Wallet
-from blockchain import Blockchain
 from socketCommunication import SocketCommunication
 from nodeAPI import NodeAPI
+from message import Message
+from blockchainUtils import BlockchainUtils
 
 class Node():
 
@@ -16,7 +18,7 @@ class Node():
 
     def startP2P(self):
         self.p2p = SocketCommunication(self.ip, self.port)
-        self.p2p.startSocketCommunication()
+        self.p2p.startSocketCommunication(self)
 
     def startAPI(self, apiPort):
         self.api = NodeAPI()
@@ -31,3 +33,6 @@ class Node():
         transactionExists = self.transactionPool.transactionExists(transaction)
         if not transactionExists and signatureValid:
             self.transactionPool.addTransaction(transaction)
+            message = Message(self.p2p.socketConnector,'TRANSACTION', transaction)
+            encodedMessage = BlockchainUtils.encode(message)
+            self.p2p.broadcast(encodedMessage)
--- socketCommunication.py
+++ socketCommunication.py
@@ -16,7 +16,8 @@ class SocketCommunication(Node):
         if self.socketConnector.port != 10001:
             self.connect_with_node('localhost', 10001)
 
-    def startSocketCommunication(self):
+    def startSocketCommunication(self, node):
+        self.node = node
         self.start()
         self.peerDiscoveryHandler.start()
         self.connectToFirstNode()
@@ -31,6 +32,9 @@ class SocketCommunication(Node):
         message = BlockchainUtils.decode(json.dumps(message))
         if message.messageType == 'DISCOVERY':
             self.peerDiscoveryHandler.handleMessage(message)
+        elif message.messageType == 'TRANSACTION':
+            transaction = message.data
+            self.node.handleTransaction(transaction)
 
     def send(self, receiver, message):
         self.send_to_node(receiver, message)
